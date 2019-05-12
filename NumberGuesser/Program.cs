0bb11882d14b070a2290c5cf5f2cd0d0506f3f61
Autumn Sellers <auresell1993@gmail.com>
2019-05-12 16:28:34 -0400

    added a quit option and got the try/catch to work, however, it prints the catch statement continuously

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -17,47 +17,51 @@ namespace NumberGuesser
 
 
             bool correctGuess = false; // This is used to exit the while loop if the answer guessed was correct
-            int guess; // This is used for the guess itself after randGuess is ran
-
             Console.WriteLine("Guess a number between 1 and 1000 then hit enter");
-
-            
+            Console.ReadLine();
 
             while (correctGuess == false) // Start the loop for guessing higher / lower
             {
-                int randGuess = r.Next(lowNumber, highNumber); // Sets  up the variable with the guess
-                guess = randGuess; // Sets up the next number it guesses
-                Console.WriteLine("Is your number lower or higher than " + guess);
-                Console.WriteLine("If the number is higher type higher. If lower type lower. If its your number type correct.");
-
-                //Console.WriteLine("-----Debug-----"); // This section was for debugging purposes. 
-                //Console.WriteLine("High: " + highNumber); // Allows me to see the current values for the to variables
-                //Console.WriteLine("Low: " + lowNumber);
-                //Console.WriteLine("----------");
-
-                string answer = Console.ReadLine();
-                
-                if (answer.ToLower() == "higher")
+                try
                 {
-                    lowNumber = guess + 1; // This sets the low end of the range + 1 because it can be assumed the number display was NOT their number
-                }
-                else if (answer.ToLower() == "lower")
-                {
-                    highNumber = guess -1; // This sets the high end of the range - 1 because it can be assumed the number display was NOT their number
-                }
-                else if (answer.ToLower() == "correct")
-                {
-                    correctGuess = true;
-                }
-                else
-                {
-                    Console.WriteLine("Please type higher, lower, or correct"); // Psuedo error checking
-                }
-            }
+                    int guess; // This is used for the guess itself after randGuess is ran
+                    int randGuess = r.Next(lowNumber, highNumber); // Sets  up the variable with the guess
+                    guess = randGuess; // Sets up the next number it guesses
+                    Console.WriteLine("Is your number lower or higher than " + guess);
+                    Console.WriteLine("If the number is higher type higher. If lower type lower. If its your number type correct.");
+                    string answer = Console.ReadLine();
 
-            Console.WriteLine("Hooray I guessed the right number!");
+                    if (answer.ToLower() == "higher")
+                    {
+                        lowNumber = guess + 1; // This sets the low end of the range + 1 because it can be assumed the number display was NOT their number
+                    }
 
+                    else if (answer.ToLower() == "lower")
+                    {
+                        highNumber = guess - 1; // This sets the high end of the range - 1 because it can be assumed the number display was NOT their number
+                    }
+                    else if (answer.ToLower() == "correct")
+                    {
+                        correctGuess = true;
+                        Console.WriteLine("Hooray I guessed the right number!");
+                    }
+                    else if (answer.ToLower() == "quit")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please type higher, lower, or correct"); // Psuedo error checking
+                    }
+                }
+                catch (ArgumentOutOfRangeException)
+                { 
+                    Console.WriteLine("Please type \"higher\" to continue");
+                    continue;
+                }
+        }
             Console.ReadLine(); // this is used to keep the console open so you can see the win phrase
         }
+
     }
 }
